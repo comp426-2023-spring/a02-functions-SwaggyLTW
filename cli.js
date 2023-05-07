d75001f264dc1326271c5954c3b8e282ac4f05c0
SwaggyLTW <tommy22@live.unc.edu>
2023-05-07 18:43:31 -0400

    third

--- cli.js
+++ cli.js
@@ -4,7 +4,6 @@ import fetch from "node-fetch";
 import minimist from "minimist"
 import moment from "moment";
 
-let timezone = moment.tz.guess();
 const args = minimist(process.argv.slice(2));
 if(args.h){
     try{
@@ -15,10 +14,13 @@ if(args.h){
         -z            Time zone: uses tz.guess() from moment-timezone by default.
         -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
         -j            Echo pretty JSON from open-meteo API and exit.`)
-        exit(0);
+        processs.exit(0);
 }catch{
-exit(1);
+process.exit(1);
 } 
+
+
+const timezone = moment.tz.guess()
 const lat = args.n || args.s;
 const long = args.e || args.w;
 const days = args.d
@@ -27,4 +29,15 @@ const days = args.d
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ lat +'&longitude='+ long + '&hourly=temperature_2m');
 const data = await response.json();
 
+if(args.j){
+    console.log(data);
+    process.exit(0)
+}
+if(days == 0){
+    console.log("today.");
+} else if(days > 1){
+    console.log("in " +days+" days.");
+} else {
+    console.log("tomorrow");
+}
 }
